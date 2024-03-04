1f8a138c1171db8da89fc1f2bc1b3be2e5907a69
erin-jones <97320633+erin-jones@users.noreply.github.com>
2024-03-04 14:37:03 -0600

    Update run_flask.sh

--- run_flask.sh
+++ run_flask.sh
@@ -4,10 +4,13 @@
 PYTHON_EXECUTABLE=/usr/bin/python3
 
 # Set the path to your Flask application file
-FLASK_APP=/home/epj36/dsci-computing/hello.py
+export FLASK_APP=/home/epj36/dsci-computing/hello.py
 
 # Activate your virtual environment if you are using one
 # source /path/to/your/virtualenv/bin/activate
 
-# Run the Flask application
-$PYTHON_EXECUTABLE $FLASK_APP
+# Optionally, set the Flask environment to development to enable debug mode
+export FLASK_ENV=development
+
+# Run the Flask application using the Flask CLI
+$PYTHON_EXECUTABLE -m flask run
