3d66321f7e3201dfdd6ec0b5a874794881521dcf
erin-jones <97320633+erin-jones@users.noreply.github.com>
2024-03-04 15:11:58 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -21,6 +21,6 @@ def name_info(name):
         return redirect(url_for('default_route'))
 
 if __name__ == "__main__":
-      app.run(host='0.0.0.0', port=80, debug=True)
+      app.run(host='0.0.0.0', port=8080, debug=True)
 
 
